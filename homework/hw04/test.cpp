99e113111e7efd53ef169a36dec6b268423107e2
Artur Hnát <hnatartu@fit.cvut.cz>
2021-04-08 16:21:14 +0200

    implement constructors and methods
    implement constructors & cpyconstructors for CRegister (deep copy) and for Car (deep copy)
    
    \nimplement CRegister::AddCard and CRegister:DelCar methods

--- homework/hw04/test.cpp
+++ homework/hw04/test.cpp
@@ -62,13 +62,13 @@ class MyVector {
     * @brief Construct a new My Vector object
     * with maximum capacity set to 0 and size 0
     */
-    MyVector() : m_max_capacity(0), m_size(0), m_buffer(nullptr) {}
+    MyVector() : m_buffer(nullptr), m_max_capacity(0), m_size(0) {}
 
     /**
      * @brief Copies a My Vector object
      * @param old source to be copied form
      */
-    MyVector(const MyVector<T>& old) : m_max_capacity(old.m_size), m_size(old.m_size), m_buffer(new T[old.m_size]) {
+    MyVector(const MyVector<T>& old) : m_buffer(new T[old.m_size]), m_max_capacity(old.m_size), m_size(old.m_size) {
         // copy contents of the old buffer to the new one
         for (int i = 0; i < old.m_size; ++i) {
             m_buffer[i] = old.m_buffer[i];
@@ -105,6 +105,16 @@ class MyVector {
         return m_buffer[index];
     };
 
+    /**
+     * @brief Overloads the [] operator
+     * Returns the T& located at the index passed in.
+     * @param index 
+     * @return T& 
+     */
+    const T& operator[](unsigned int index) const {
+        return m_buffer[index];
+    };
+
     // ====================================== METHODS ======================================
     /**
      * @brief Adds a value to our container.
@@ -133,7 +143,7 @@ class MyVector {
      * @brief Returns the size of our container.
      * @return unsigned int 
      */
-    unsigned int size() { return m_size; };
+    unsigned int size() const { return m_size; };
 
     /**
      * @brief Returns the last element from our container.
@@ -160,11 +170,22 @@ class MyVector {
         m_size++;
     }
 
+    void erase(const unsigned int position) {
+        delete &m_buffer[position];
+        m_size--;
+        // fill the deleted position
+        for (int i = (int)position; i < m_size; i++) {
+            m_buffer[i] = m_buffer[i + 1];
+        }
+    }
+
     /** @brief Utility method to print the vector on the standard output. */
     void print() {
+        std::cout << "[ " << endl;
         for (int i = 0; i < m_size; ++i) {
-            std::cout << m_buffer[i] << std::endl;
+            std::cout << "\t" << m_buffer[i] << std::endl;
         }
+        std::cout << "] " << endl;
     }
 };
 
@@ -191,13 +212,136 @@ class COwnerList {
     // todo
 };
 
-class CRegister {
+class Car {
    public:
-    // copy cons, op=, dtor ...
+    char* rz;
+    char* name;
+    char* surname;
+
+    Car() : rz(nullptr), name(nullptr), surname(nullptr) {}
+
+    Car(const char* _rz, const char* _name, const char* _surname) {
+        rz = new char[strlen(_rz) + 1];
+        strcpy(rz, _rz);
+
+        name = new char[strlen(_name) + 1];
+        strcpy(name, _name);
+
+        surname = new char[strlen(_surname) + 1];
+        strcpy(surname, _surname);
+    }
+
+    ~Car() {
+        delete rz;
+        delete name;
+        delete surname;
+    }
+
+    Car(const Car& old) {
+        rz = new char[strlen(old.rz) + 1];
+        strcpy(rz, old.rz);
+
+        name = new char[strlen(old.name) + 1];
+        strcpy(name, old.name);
+
+        surname = new char[strlen(old.surname) + 1];
+        strcpy(surname, old.surname);
+    }
+
+    Car& operator=(Car old) {
+        std::swap(rz, old.rz);
+        std::swap(name, old.name);
+        std::swap(surname, old.surname);
+        return *this;
+    }
+
+    friend ostream& operator<<(ostream& os, const Car& c) {
+        os << "[" << c.rz << "] - owned by: " << c.name << " " << c.surname;
+        return os;
+    }
+};
+
+class CRegister {
+   private:
+    MyVector<Car> m_data;
+
+   public:
+    CRegister() = default;
+    ~CRegister() = default;
+
+    CRegister(const CRegister& old) {
+        for (int i = 0; i < old.m_data.size(); i++) {
+            m_data.push_back(old.m_data[i]);
+        }
+    }
+
+    CRegister& operator=(CRegister old) {
+        std::swap(m_data, old.m_data);
+        return *this;
+    }
+
+    /**
+     * @brief Adds a car to the registry.
+     * @param rz license plate number
+     * @param name first name of the owner
+     * @param surname last name of the owner
+     * @return true When added successfully
+     * @return false When failed to add
+     */
     bool AddCar(const char* rz,
                 const char* name,
-                const char* surname);
-    bool DelCar(const char* rz);
+                const char* surname) {
+        // check if car with the same rz exists
+        for (int i = 0; i < m_data.size(); i++) {
+            if (strcmp(rz, m_data[i].rz) == 0) {
+                // if it does exist, return false
+                return false;
+            }
+        }
+        // otherwise add it
+
+        cout << "============================================================" << endl;
+        cout << "Before adding:" << endl;
+        m_data.print();
+        cout << "=============== Adding rz: '" << rz << "' ===============" << endl;
+
+        m_data.push_back(Car(rz, name, surname));
+
+        cout << "After adding:" << endl;
+        m_data.print();
+        cout << "============================================================" << endl;
+
+        // TODO is this ^ correct?
+        return true;
+    }
+
+    /**
+     * @brief Deletes a car from the registry.
+     * @param rz license plate number
+     * @return true When found and deleted
+     * @return false When not found
+     */
+    bool DelCar(const char* rz) {
+        // find the car with the same rz
+        for (int i = 0; i < m_data.size(); i++) {
+            if (strcmp(rz, m_data[i].rz) == 0) {
+                // when found, remove it
+                cout << "============================================================" << endl;
+                cout << "Before delete:" << endl;
+                m_data.print();
+
+                cout << "=============== DELETING rz: '" << m_data[i].rz << "' ===============" << endl;
+                m_data.erase(i);
+
+                cout << "After delete:" << endl;
+                m_data.print();
+                cout << "============================================================" << endl;
+
+                return true;
+            }
+        }
+        return false;
+    }
     bool Transfer(const char* rz,
                   const char* nName,
                   const char* nSurname);
@@ -207,9 +351,6 @@ class CRegister {
                   const char* surname) const;
     COwnerList ListOwners(const char* RZ) const;
     int CountOwners(const char* RZ) const;
-
-   private:
-    // todo
 };
 
 #ifndef __PROGTEST__
@@ -235,6 +376,9 @@ int main(void) {
     strncpy(name, "Peter", sizeof(name));
     strncpy(surname, "Smith", sizeof(surname));
     assert(b0.AddCar("XYZ-11-22", name, surname) == true);
+
+    return 0;
+
     assert(b0.CountCars("John", "Hacker") == 1);
     assert(matchList(b0.ListCars("John", "Hacker"), "ABC-32-22"));
     assert(b0.CountOwners("ABC-12-34") == 1);
