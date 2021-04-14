aa4d2b01c77ecd87c6e83dab2b5f0bfdecbbcbab
Artur Hnát <hnatartu@fit.cvut.cz>
2021-04-14 13:58:29 +0200

    [HW05] - Implement CStudent class and CExam::Load method

--- homework/hw05/anonymous-tests.cpp
+++ homework/hw05/anonymous-tests.cpp
@@ -28,9 +28,78 @@ class CResult {
 
 #endif /* __PROGTEST__ */
 
+class CStudent {
+   private:
+    unsigned int m_id;
+    string m_fullname;
+    vector<string> m_cards;
+    CResult* m_result;
+
+   public:
+    CStudent() = delete;
+    CStudent(const string& line) : m_result(nullptr) {
+        istringstream s(line);
+        string field;
+        int index = 0;  // save index so we know which field we're reading
+        // process the line
+        while (getline(s, field, ':')) {
+            // process the field
+            switch (index) {
+                case 0: {
+                    // we're parsing the student_id
+                    size_t* check = nullptr;
+                    m_id = stoul(field, check, 10);
+                    if (check != nullptr) {
+                        cout << "Failed to parse Student ID! '" << *check << "' is not an unsinged int.";
+                    }
+                    index += 1;
+                    break;
+                }
+                case 1: {
+                    // we're parsing the full name
+                    m_fullname = field;
+                    index += 1;
+                    break;
+                }
+                default: {
+                    // we're parsing card id
+                    m_cards.push_back(field);
+                    index += 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    const unsigned int Id() const { return m_id; }
+    const string& Name() const { return m_fullname; }
+    const vector<string>& Cards() const { return m_cards; }
+
+    void Print() {
+        cout << m_fullname << " (" << m_id << ") ";
+        cout << "Cards: [";
+        for (const auto& it : m_cards) {
+            cout << it << ", ";
+        }
+        cout << "]" << endl;
+    }
+};
+
 class CExam {
    private:
-    // todo
+    vector<CStudent*> m_students;
+
+    /** @brief Map with ids as keys and pointer to the students the id belongs as the value */
+    map<unsigned int, CStudent*> m_students_by_id;
+
+    /** @brief Map with cards as keys and pointer to the students the card belongs as the value */
+    map<string, CStudent*> m_students_by_card;
+
+    /**
+     * @brief Container to keep data about the current exam results.
+     * Time is the time when the student's result was assessed
+     */
+    map<time_t, CStudent*> m_students_by_assess;
 
    public:
     static const int SORT_NONE = 0;
@@ -43,16 +112,56 @@ class CExam {
      * Supposed to be implicit, so we keep it set to default.
      */
     CExam() = default;
+    ~CExam() {
+        for (auto it : m_students) {
+            delete it;
+        }
+    }
+    bool Load(istream& cardMap) {
+        // TODO do i have to load students into TMP and drop them when one in the stream fails?
 
-    bool Load(istream& cardMap);
+        string line;
+        // read stream line by line
+        while (getline(cardMap, line)) {
+            CStudent* student = new CStudent(line);
+            // check if students id isn't already present
+            if (m_students_by_id.count(student->Id()) > 0) {
+                cout << "Student ID " << student->Id() << " is already present in this exam!";
+                delete student;
+                return false;
+            }
 
-    bool Register(const string& cardID, const string& test);
+            // check if any of student's cards aren't already present
+            auto cards = student->Cards();
+            for (auto const& it : cards) {
+                if (m_students_by_card.count(it) > 0) {
+                    cout << "Student card " << it << " is already present in this exam!";
+                    delete student;
+                    return false;
+                }
+            }
+            m_students.push_back(student);
+            m_students_by_id.insert(make_pair(student->Id(), student));
+            for (auto const& it : cards) {
+                m_students_by_card.insert(make_pair(it, student));
+            }
+        }
+        return true;
+    }
 
-    bool Assess(unsigned int studentID, const string& test, int result);
+    bool Register(const string& cardID, const string& test) { return true; }
 
-    list<CResult> ListTest(const string& testName, int sortBy) const;
+    bool Assess(unsigned int studentID, const string& test, int result) { return true; }
 
-    set<unsigned int> ListMissing(const string& testName) const;
+    list<CResult> ListTest(const string& testName, int sortBy) const {
+        list<CResult> l;
+        return l;
+    }
+
+    set<unsigned int> ListMissing(const string& testName) const {
+        set<unsigned int> s;
+        return s;
+    }
 };
 
 #ifndef __PROGTEST__
