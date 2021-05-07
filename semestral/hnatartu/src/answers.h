21a3b6397319d683e230e5f1782aadf977087d04
Artur Hnát <hnatartu@fit.cvut.cz>
2021-05-07 18:13:25 +0200

    [ANSWERS] - Add a few classes to represent an Answer

--- semestral/hnatartu/src/answers.h
+++ semestral/hnatartu/src/answers.h
@@ -1,4 +1,24 @@
 #ifndef __ANSWERS_H__
 #define __ANSWERS_H__
 
+#include <string>
+
+using namespace std;
+class CAnswerBase {
+   private:
+    string m_Type;
+
+   public:
+    CAnswerBase();
+    CAnswerBase(const string& type);
+    virtual ~CAnswerBase();
+
+    const string& getType();
+    void setType(const string& type);
+};
+
+class CExactMatch {};
+class CFromSet {};
+class CPattern {};
+
 #endif  // __ANSWERS_H__
