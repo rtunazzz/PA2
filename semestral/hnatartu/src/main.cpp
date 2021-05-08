1dd328fa9080c7c122c02bc3e7978f7cd7d55330
Artur Hnát <hnatartu@fit.cvut.cz>
2021-05-08 11:58:03 +0200

    [MAIN] - Include window files and init window

--- semestral/hnatartu/src/main.cpp
+++ semestral/hnatartu/src/main.cpp
@@ -1,41 +1,7 @@
-#include <SDL2/SDL.h>
+#include "window.h"
 
-#include <iostream>
-
-int main(int argv, char **args) {
-    SDL_Init(SDL_INIT_EVERYTHING);
-
-    SDL_Window *window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-
-    bool isRunning = true;
-    SDL_Event event;
-
-    int i = 0;
-    while (isRunning) {
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_QUIT:
-                    isRunning = false;
-                    break;
-
-                case SDL_KEYDOWN:
-                    if (event.key.keysym.sym == SDLK_ESCAPE) {
-                        isRunning = false;
-                    }
-            }
-        }
-
-        SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, i, 256 - i, i, i);
-
-        SDL_RenderPresent(renderer);
-        i += 1;
-    }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
+int main(int argc, char *argv[]) {
+    CSDLWindow quizWindow = CSDLWindow();
+    quizWindow.run();
     return 0;
 }
