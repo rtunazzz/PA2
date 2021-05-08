bded3ffa2dbb3b90e3acb9553d7d98e16f48a84b
Artur Hnát <hnatartu@fit.cvut.cz>
2021-05-08 11:57:02 +0200

    [ANSWERS] - Add comments and methods to answers

--- semestral/hnatartu/src/answers.h
+++ semestral/hnatartu/src/answers.h
@@ -11,14 +11,35 @@ class CAnswerBase {
    public:
     CAnswerBase();
     CAnswerBase(const string& type);
+
+    /**
+     * @brief Construct a new CAnswerBase object from its string representation
+     * @param stringRepr String (text) representation of the answer
+     */
+    // CAnswerBase(const string& stringRepr);
+
     virtual ~CAnswerBase();
 
     const string& getType();
     void setType(const string& type);
+
+    /**
+     * @brief Checks whether or not the answer provided is correct.
+     * @param answer Answer to check
+     * @return Whether or not is the answer provided correct
+     */
+    virtual bool isCorrect(const string& answer);
+
+    /**
+     * @brief Converts this answer to a string representation.
+     * This string representation can be then used for import and export.
+     * @return string String (text) representation
+     */
+    virtual string toString();
 };
 
-class CExactMatch {};
-class CFromSet {};
-class CPattern {};
+class CExactMatch : CAnswerBase {};
+class CPattern : CAnswerBase {};
+class CFromSet : CAnswerBase {};
 
 #endif  // __ANSWERS_H__
